9d6ed9611f58e57148ccd62eeedb9e26c14e490f
AcidToast76 <letukasjosh77@gmail.com>
2024-06-19 02:07:38 +0100

    finished tank program!!!

--- index.py
+++ index.py
@@ -19,30 +19,36 @@ for d1 in range(8):
         print("-", end=" ")
     print()
 
-
 coordinatex = int(input("Enter the x coordinate: "))
-
 coordinatey = int(input("Enter the y coordinate: "))
 
-def function():
-    if x[coordinatex][coordinatey] == "T":
+def function1(coordinatex, coordinatey):
+
+    if coordinatex < 0 or coordinatex > 7 or coordinatey < 0 or coordinatey > 7 or coordinatex == "" or coordinatey == "":
+        print("Please enter a valid coordinate!")
+        coordinatex = int(input("Enter the x coordinate: "))
+        coordinatey = int(input("Enter the y coordinate: "))
+        return False
+    elif x[coordinatex][coordinatey] == "T":
         p = 1
-        print("Congratulations! You have found", p, "treasure(s)!")
+        print("Congratulations! You have found", p, "tank(s)!")
         p =+ 1
         return True
     else:
-        print("Sorry, you have not found any treasure.")
+        print("Sorry, there is no tank at", coordinatex, coordinatey, "coordinates!")
         print("Try again!")
         return False
+
+
 
 o = 0
 
 while o < 10:
-    if function() == True:
+    if function1(coordinatex, coordinatey) == True:
         o = o
-        coordinatex = int(input("Enter the x coordinate: ")) + 1
-        coordinatey = int(input("Enter the y coordinate: ")) + 1
-    else:
+        coordinatex = int(input("Enter the x coordinate: "))
+        coordinatey = int(input("Enter the y coordinate: "))
+    elif function1(coordinatex, coordinatey) == False and coordinatex >= 0 and coordinatex <= 7 and coordinatey >= 0 and coordinatey <= 7:
         print("You have", 10 - o, "tries left.")
         o += 1
         coordinatex = int(input("Enter the x coordinate: "))
